cb68a4ee9bfc4dbdab61e9ce02525afc8a0d3a5c
Rudolfs <rudolfs@eglitis.name>
2011-06-27 14:52:56 +0300

    Validation! (+title min 10 chars)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,9 @@
 class Product < ActiveRecord::Base
-  validates :title, :uniqueness => true
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
   validates :image_url, :format => {
     :with     => %r{\.(gif|jpg|png)$}i,
     :message  => 'must be URL for GIF, JPG or PNG image.'
   }
+  validates :title, :uniqueness => true, :length =>  { :minimum => 10, :message => "must be at least 10 characters long" }
 end
