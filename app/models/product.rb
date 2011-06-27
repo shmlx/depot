f5da3cb9ee70be3b9e1e45b3b687af521864db62
Rudolfs <rudolfs@eglitis.name>
2011-06-27 14:44:06 +0300

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :uniqueness => true
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
+  validates :image_url, :format => {
+    :with     => %r{\.(gif|jpg|png)$}i,
+    :message  => 'must be URL for GIF, JPG or PNG image.'
+  }
 end
