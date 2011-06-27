d6ac927eea0480cdbc1147699172f7909e5c60df
Rudolfs <rudolfs@eglitis.name>
2011-06-27 17:36:57 +0300

    pirmdiena, bez playtime @ 125lpp

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
@@ -8,4 +10,15 @@ class Product < ActiveRecord::Base
     :message  => 'must be URL for GIF, JPG or PNG image.'
   }
   validates :title, :uniqueness => true, :length =>  { :minimum => 10, :message => "must be at least 10 characters long" }
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        true
+      else
+        errors.add(:base, 'Line Items present')
+        false
+      end
+    end
 end
