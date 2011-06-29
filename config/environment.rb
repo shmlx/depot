24526998a36d2506310f5a977933af704c9810bb
Rudolfs Eglitis <shmlx@shmlx.info>
2011-06-29 17:48:58 +0300

    liidz 208lpp

--- config/environment.rb
+++ config/environment.rb
@@ -5,15 +5,20 @@ require File.expand_path('../application', __FILE__)
 Depot::Application.initialize!
 
 Depot::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "domain.of.sender.net",
-    :authentication       => "plain",
-    :user_name            => "dave",
-    :password             => "secret",
-    :enable_starttls_auto => true
-  }
-end
+    require 'tlsmail'
+      Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+      ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.raise_delivery_errors = true
+      ActionMailer::Base.smtp_settings = {
+          :address => "smtp.gmail.com",
+          :port => "587",
+          :domain => "shmlx.info",
+          :enable_starttls_auto => true,
+          :authentication => :login,
+          :user_name => "ruby@shmlx.info",
+          :password => "ruby!!!!"
+      }
 
+    config.action_mailer.raise_delivery_errors = true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Depot::Application.routes.draw do
 
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
+
   resources :orders
 
   match "line_items/decrement", :to=> 'line_items#decrement', :method=>:post
