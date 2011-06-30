35d0a33413ce85eedf89efab97538e4038d98b9e
Rudolfs Eglitis <shmlx@shmlx.info>
2011-06-30 17:58:34 +0300

    natin much

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   before_filter :set_i18n_locale_from_params
-  #before_filter :convert_price_to_lvl
   before_filter :authorize
   protect_from_forgery
 
